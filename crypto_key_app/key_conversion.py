1e7f6e157988c4c3832a69f31a825c79732b0a54
pjh1cob <Padmanathan.Snehavalli@in.bosch.com>
2024-10-29 09:47:43 +0530

    Public key - pem to hex added

--- crypto_key_app/key_conversion.py
+++ crypto_key_app/key_conversion.py
@@ -1,9 +1,9 @@
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+import key_management as keys
 
 # Convert PEM key to hex
-
-# TO DO : add support to public keys
 def pem_to_hex(pem_key):
     if isinstance(pem_key, rsa.RSAPrivateKey):
         pem_bytes = pem_key.private_bytes(
@@ -12,10 +12,21 @@ def pem_to_hex(pem_key):
             encryption_algorithm=serialization.NoEncryption()
         )
         return pem_bytes.hex()
+    elif isinstance(pem_key, rsa.RSAPublicKey):
+        pem_bytes = pem_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return pem_bytes.hex()
     else:
-        print("Can only convert secret keys to hex")
+        raise ValueError("Can only convert RSA private or public keys to hex")
+
 
 # Convert hex back to PEM
 def hex_to_pem(hex_key):
-    pem_bytes = bytes.fromhex(hex_key)
-    return pem_bytes
+    try:
+        pem_bytes = bytes.fromhex(hex_key)
+        return pem_bytes
+    except ValueError:
+        raise ValueError("Invalid hexadecimal string")
+    
